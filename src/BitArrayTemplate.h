63013fed0998993f98269cea7eb3665cf6c24d84
auron-muc <github@auron.de>
2015-12-20 22:40:43 +0100

    Fixed read bug in BitArray

--- src/BitArrayTemplate.h
+++ src/BitArrayTemplate.h
@@ -85,19 +85,17 @@ void BitArrayTemplate<MAX_BITS>::set(const uint8_t &offset, const int &num_bits,
 template <uint16_t MAX_BITS>
 void BitArrayTemplate<MAX_BITS>::set(const uint8_t &offset, const int &num_bits, uint16_t value)
 {
-	if (num_bits>8) {
-		set(offset  ,8          ,(uint8_t)(value&0xff));
-		set(offset+8,num_bits-8 ,(uint8_t)(value>>8));
-	} else {
-		set(offset  ,num_bits   ,(uint8_t)(value&0xff));
-	}	
+	for (int i=0; i <num_bits; i++) {
+		set(offset+i, (value & 1));
+		value = value >> 1;
+	}
 }
 
 template <uint16_t MAX_BITS>
 void BitArrayTemplate<MAX_BITS>::set(const uint8_t &offset, const int &num_bits, uint32_t value)
 {
-	for (int i=num_bits; i>0; i--) {
-		set(offset+i-1, (value & 1));
+	for (int i=0; i <num_bits; i++) {
+		set(offset+i, (value & 1));
 		value = value >> 1;
 	}
 }
@@ -114,8 +112,8 @@ template <uint16_t MAX_BITS>
 uint8_t BitArrayTemplate<MAX_BITS>::getByte(const uint8_t &offset, const int &num_bits)
 {
 	uint8_t result = 0;
-	for (int i = 0 ; i <num_bits; i++) {
-		result = (result <<1) | getBit(offset+i);
+	for (int i = num_bits ; i>0; i--) {
+		result = (result <<1) | getBit(offset+i-1);
 	}
 	return result;
 }
@@ -124,8 +122,8 @@ template <uint16_t MAX_BITS>
 uint16_t BitArrayTemplate<MAX_BITS>::getWord(const uint8_t &offset, const int &num_bits)
 {
 	uint16_t result = 0;
-	for (int i = 0 ; i <num_bits; i++) {
-		result = (result <<1) | getBit(offset+i);
+	for (int i = num_bits ; i>0; i--) {
+		result = (result <<1) | getBit(offset+i-1);
 	}
 	return result;
 }
@@ -134,8 +132,8 @@ template <uint16_t MAX_BITS>
 uint32_t BitArrayTemplate<MAX_BITS>::getDWord(const uint8_t &offset, const int &num_bits)
 {
 	uint32_t result = 0;
-	for (int i = 0 ; i <num_bits; i++) {
-		result = (result <<1) | getBit(offset+i);
+	for (int i = num_bits ; i>0; i--) {
+		result = (result <<1) | getBit(offset+i-1);
 	}
 	return result;
 }
